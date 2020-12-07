c27368171315bce3b2feb1db8e6cf23fe730bb8a
Aditya8108 <74955177+Aditya8108@users.noreply.github.com>
2020-12-07 13:31:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,59 +1,49 @@
-
-var fixedRect, movingRect;
-var o1,o2,o3;
+var bullet,wall;
+var speed, weight,thickness;
 
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-  o1=createSprite(70,70,100,50);
-  o2=createSprite(70,170,100,50);
-  o3=createSprite(300,250,50,100);
-  o1.shapeColor="green";
-  o2.shapeColor="green";
-  o3.shapeColor="green";
+  createCanvas(1300,400);
+
+  speed=random(30,52);
+  weight=random(223,321);
+  thickness=random(22,83);
+
+  bullet=createSprite(50, 200, 40, 10);
+  bullet.shapeColor=color(225);
+  bullet.velocityX=speed;
+
+  wall=createSprite(1200,200,thickness,height/2);
 }
 
 function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+  background("black");
 
- if( isTouching(movingRect,o1)){
-  movingRect.shapeColor = "blue";
-  o1.shapeColor = "blue";}
- else{
-  movingRect.shapeColor = "green";
-  o1.shapeColor = "green";
- }
+    if(hasCollided(bullet, wall))
+    {
+      bullet.velocityX=0;
+      var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+      if(damage>10)
+      {
+        wall.shapeColor=color(230,0,0);
+      }
 
- if( isTouching(movingRect,o2)){
-  movingRect.shapeColor = "blue";
-  o2.shapeColor = "blue";}
- else{
-  movingRect.shapeColor = "green";
-  o2.shapeColor = "green";
- }
-
- if( isTouching(movingRect,fixedRect)){
-  movingRect.shapeColor = "blue";
-  fixedRect.shapeColor = "blue";}
- else{
-  movingRect.shapeColor = "green";
-  fixedRect.shapeColor = "green";
- }
-
- if( isTouching(movingRect,o3)){
-  movingRect.shapeColor = "blue";
-  o3.shapeColor = "blue";}
- else{
-  movingRect.shapeColor = "green";
-  o3.shapeColor = "green";
- }
+      if(damage<10)
+      {
+        wall.shapeColor=color(0,255,0);
+      }
+    }
 
   drawSprites();
+}
+
+function hasCollided(Lbullet,Lwall)
+{
+  bulletRightEdge=Lbullet.x+Lbullet.width;
+  wallLeftEdge=Lwall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true;
+  }
+return false;;
+
 }
